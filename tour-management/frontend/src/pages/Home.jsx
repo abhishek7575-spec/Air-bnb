e4c908aa89799a65ecbc4d7985eded6720ddfa58
abhishek7575-spec <abhikrishna185@gmail.com>
2023-03-21 02:48:21 +0530

    home page added with image and video

--- tour-management/frontend/src/pages/Home.jsx
+++ tour-management/frontend/src/pages/Home.jsx
@@ -1,9 +1,72 @@
 import React from 'react'
 import '../styles/home.css'
+import {Container ,Row ,Col} from 'reactstrap'
+import heroImg from '../assets/images/hero-img01.jpg'
+import heroImg02 from '../assets/images/hero-img02.jpg'
+import heroVideo from '../assets/images/hero-video.mp4'
+import worldImg from '../assets/images/world.png'
+import Subtitle from './../shared/Subtitle'
 
-const Home = () => {
+import SearchBar from '../shared/SearchBar'
+import ServiceList from '../services/ServiceList'
+
+
+const Home = () => { 
   return (
-    <div>Home</div>
+    <>
+      <section>
+        <Container>
+          <Row>
+            <Col lg='6'>
+                <div className="hero__content">
+                      <div className="hero__subtitle d-flex align-items-center  ">
+                          <Subtitle subtitle={'Know Before You Go'}/>
+                          <img src={worldImg} alt="" />
+                      </div>
+                      <h1>
+                        Traveling opens the door to creating{" "} 
+                        <span className='highlight'>memories</span>
+                      </h1>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia, at! Quos modi voluptatibus iste hic in, vel delectus harum fuga laudantium provident officiis architecto dolore inventore praesentium totam aliquid. Quos!
+                        Temporibus, saepe quisquam modi nisi repellendus beatae dolorem itaque praesentium eligendi pariatur lab
+                      </p>
+                </div>
+            </Col>
+            <Col lg='2'>
+              <div className='hero__img-box'>
+                <img src={heroImg} alt="" />
+              </div>
+            </Col>
+            <Col lg='2'>
+              <div className='hero__img-box mt-4'>
+                <video src={heroVideo} alt="" controls/>
+              </div>
+            </Col>
+            <Col lg='2'>
+              <div className='hero__img-box mt-5'>
+                <img src={heroImg02} alt="" />
+              </div>
+            </Col>
+          <SearchBar/>
+          </Row>
+        </Container>
+      </section>
+      {/* hero section start*/}
+        <section>
+          <Container>
+            <Row>
+              <Col lg='3'>
+                  <h5 className="services__subtitle">What we serve</h5>
+                  <h2 className="services__title">We offer our best services</h2>
+
+              </Col>
+              <ServiceList/>
+            </Row>
+          </Container>
+        </section>
+      {/* hero section end*/}
+    </>
   )
 }
 

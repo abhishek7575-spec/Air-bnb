21c732ef1f160407f5f95d652ea9fc4b744ae4d2
abhishek7575-spec <abhikrishna185@gmail.com>
2023-03-27 16:01:19 +0530

    tour page made

--- tour-management/frontend/src/pages/TourDetails.jsx
+++ tour-management/frontend/src/pages/TourDetails.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import '../styles/tour-details.css'
+import {Container,Row,Col,Form,ListGroup} from "reactstrap"
+import {useParams} from 'react-router-dom'
+import tourData from '../assets/data/tours'
 const TourDetails = () => {
+    const {id} =useParams();
+
+    // later use backend  
+    const tour=tourData.find(tour=>tour.id===id)
+
+    const {photo,title,desc,price,reviews,city,distance,maxGroupSize}=tour
+
   return (
-    <div>TourDetails</div>
+    <>
+      <Container>
+        <Row>
+          <Col lg='8'>
+            <div className="tour__content">
+              <img src="" alt="" />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
   )
 }
 

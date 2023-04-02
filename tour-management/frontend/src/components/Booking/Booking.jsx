22a3d4bd2c25d868958ecaf14895634f124b9370
abhishek7575-spec <abhikrishna185@gmail.com>
2023-04-02 13:23:57 +0530

    booking card done

--- tour-management/frontend/src/components/Booking/Booking.jsx
+++ tour-management/frontend/src/components/Booking/Booking.jsx
@@ -5,6 +5,9 @@ const Booking = ({tour,avgRating}) => {
 
     const {price,reviews}=tour
 
+  const handleChange=e=>{
+
+  }
 
   return (<>
   <div className="booking">
@@ -16,7 +19,26 @@ const Booking = ({tour,avgRating}) => {
    </span>
   </div>
   {/* booking form */}
-        
+        <div className="booking__form">
+          <h5>Information</h5>
+          <Form className='booking__info-form'>
+            <FormGroup>
+              <input type="text" placeholder="Full Name" id="fullName" required
+              onChange={handleChange} />
+            </FormGroup>
+            <FormGroup>
+              <input type="number" placeholder="Phone" id="phone" required
+              onChange={handleChange} />
+            </FormGroup>
+            <FormGroup className='d-flex align-items-center gap-3'>
+              <input type="date" placeholder="" id="bookAt" required
+              onChange={handleChange} />
+              <input type="number" placeholder="Guest" id="guestSize" required
+              onChange={handleChange} />
+            </FormGroup>
+
+          </Form>
+        </div>
   {/* booking form end */}
   </div>
   </>
